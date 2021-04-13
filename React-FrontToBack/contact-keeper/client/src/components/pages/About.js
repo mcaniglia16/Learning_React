b41914f8c9c7818476f5e8d08911e4ded760dac2
mcaniglia16 <50206147+mcaniglia16@users.noreply.github.com>
2021-04-13 12:33:11 -0400

    added alerts & worked on auth

--- React-FrontToBack/contact-keeper/client/src/components/pages/About.js
+++ React-FrontToBack/contact-keeper/client/src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <div>
             <h1>About This App</h1>
             <p className="my-1">
-                This is a full stack React app to manage your Contact
+                This is a full stack React app to manage your contacts
             </p>
             <p className="bg-dark p">
                 <strong>Version:</strong> 1.0.0
