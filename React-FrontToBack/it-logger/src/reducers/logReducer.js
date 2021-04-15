032183864cc479dbbe06f57159ad5b29e6f6077c
mcaniglia16 <50206147+mcaniglia16@users.noreply.github.com>
2021-04-15 18:53:28 -0400

    moving Logs functionality out of component into reducer

--- React-FrontToBack/it-logger/src/reducers/logReducer.js
+++ React-FrontToBack/it-logger/src/reducers/logReducer.js
@@ -9,6 +9,18 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case GET_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
+        loading: false,
+      };
+    case LOGS_ERROR:
+      console.log(action.payload);
+      return {
+        ...state,
+        error: action.payload,
+      };
     case SET_LOADING:
       return {
         ...state,
