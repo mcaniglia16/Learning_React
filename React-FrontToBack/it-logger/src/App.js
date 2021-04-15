7eda7ac3044e9aa1a4358b797ef64f1319a293db
mcaniglia16 <50206147+mcaniglia16@users.noreply.github.com>
2021-04-15 17:37:55 -0400

    started implementing state management with redux

--- React-FrontToBack/it-logger/src/App.js
+++ React-FrontToBack/it-logger/src/App.js
@@ -1,9 +1,16 @@
 import React, { useEffect, Fragment } from "react";
-import "materialize-css/dist/css/materialize.min.css";
-import M from "materialize-css/dist/js/materialize.min.js";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 
 const App = () => {
   useEffect(() => {
@@ -12,14 +19,20 @@ const App = () => {
   });
 
   return (
-    <Fragment>
-      <SearchBar />
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
 
-      <div className="container">
-        <AddBtn />
-        <Logs />
-      </div>
-    </Fragment>
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
